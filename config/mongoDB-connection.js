9884035ec6383abcb27b07270ad9c5e4487165ed
hafsa <nadeemhafsa704@gmail.com>
2025-08-02 15:19:48 +0500

    profile info rendering via ejs

--- config/mongoDB-connection.js
+++ config/mongoDB-connection.js
@@ -1,32 +1,32 @@
-// const mongoose = require('mongoose');
-// const debug = require('debug')('development:mongoose');
-// require('dotenv').config();
+// db.js
+const mongoose = require('mongoose');
+const debug = require('debug')('development:mongoose');
+require('dotenv').config();
 
-// const MONGO_URI = process.env.MONGODB_URI;
+const MONGO_URI = process.env.MONGODB_URI;
 
-// if (!MONGO_URI) {
-//   throw new Error(' MONGODB_URI is not defined in .env file');
-// }
+if (!MONGO_URI) {
+  throw new Error('MONGODB_URI is not defined in the .env file');
+}
 
-// mongoose.connect(MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
-//   .then(() => debug(' MongoDB CONNECTED'))
-//   .catch((err) => {
-//     debug(' MongoDB CONNECTION ERROR:', err.message);
-//     process.exit(1); //  exit if db is required to run the app
-//   });
+// Connect to MongoDB
+mongoose.connect(MONGO_URI)
+  .then(() => debug(' MongoDB connected successfully'))
+  .catch((err) => {
+    debug(' MongoDB connection error:', err.message);
+    process.exit(1); // Stop the app if DB is required
+  });
 
-// //  log disconnects or other events
-// mongoose.connection.on('disconnected', () => {
-//   debug('MongoDB DISCONNECTED');
-// });
+//  Listen for disconnects
+mongoose.connection.on('disconnected', () => {
+  debug('⚠️ MongoDB disconnected');
+});
 
-// process.on('SIGINT', async () => {
-//   await mongoose.connection.close();
-//   debug(' MongoDB connection closed due to app termination');
-//   process.exit(0);
-// });
+//  handle app termination
+process.on('SIGINT', async () => {
+  await mongoose.connection.close();
+  debug('💡 MongoDB connection closed due to app termination');
+  process.exit(0);
+});
 
-// module.exports = mongoose.connection;
+module.exports = mongoose.connection;
