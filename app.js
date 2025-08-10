1b675abac9f5bd48d88873fa97b40eb8460fd225
hafsa <nadeemhafsa704@gmail.com>
2025-08-10 20:13:26 +0500

    / route

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/pinterest/pin', pinRouter);
 app.use('/pinterest/boards',boardRouter)
 
 app.get('/',(req,res)=>{
-  res.render('login')
+  res.render('home')
 })
 
 const helmet = require('helmet');
