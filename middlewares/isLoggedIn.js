279a4357171a83f353879f3db464e4e2959b5dba
hafsa <nadeemhafsa704@gmail.com>
2025-08-12 21:26:26 +0500

    redirection

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -1,10 +1,10 @@
 // middleware/isLoggedIn.js
 
- function isLoggedIn(req, res, next) {
+function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
-    return next(); // User is logged in
+    return next();
   }
-  res.redirect('/pinterest/login'); // Redirect if not authenticated
-};
+  res.status(401).send('MUST LOGIN TO GET ACCESS TO THIS ROUTE');
+}
 
- module.exports= {isLoggedIn}
+module.exports = { isLoggedIn };
